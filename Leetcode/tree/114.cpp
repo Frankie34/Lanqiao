03995fa64039c71c28cf3315d6cee94791e9f334
Juncheng Zhu <zhujuncheng1021@163.com>
2019-04-26 21:33:44 +0800

    more exercise

---
+++ Leetcode/tree/114.cpp
@@ -0,0 +1,10 @@
+void flatten(TreeNode *root) {
+        if (!root) return;
+        if (root->left) flatten(root->left);
+        if (root->right) flatten(root->right);
+        TreeNode *tmp = root->right;
+        root->right = root->left;
+        root->left = NULL;
+        while (root->right) root = root->right;
+        root->right = tmp;
+    }
